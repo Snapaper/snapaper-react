8c5828886978aa400b750858de697fa3c0cba81d
HelipengTony <he@holptech.com>
2020-05-02 03:24:21 +0800

    Chore: a few updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,32 +1,50 @@
 import { HeartFilled, GithubFilled, UserOutlined } from "@ant-design/icons";
-import { useRouter } from 'next/router'
+import React from "react";
+import { withRouter } from "next/router";
 
 // useRouter 只可在 function 中调用(React Hook 特征)
-function Footer({ children, href }) {
-  const router = useRouter();
-  return (
-    <footer
-      className={router.route == "/" ? "footer-div" : "footer-div-bottom"}
-    >
-      <div className="footer-container">
-        <div>
-          <p>
-            &copy; Copyright 2018-2020 Snapaper · Made with <HeartFilled /> for
-            the People of the Internet · 蜀ICP备16017417号-10
-          </p>
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: props.router.route,
+      loading: props.loading,
+    };
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.loading !== this.state.loading) {
+      this.state.loading = prevProps.loading;
+    }
+  }
+  render() {
+    return (
+      <footer
+        className={
+          this.state.page == "/" || this.props.loading
+            ? "footer-div"
+            : "footer-div-bottom"
+        }
+      >
+        <div className="footer-container">
+          <div>
+            <p>
+              &copy; Copyright 2018-2020 Snapaper · Made with <HeartFilled />{" "}
+              for the People of the Internet · 蜀ICP备16017417号-10
+            </p>
+          </div>
+          <div>
+            <a href="https://github.com/Snapaper" target="_blank">
+              <GithubFilled /> Open-source
+            </a>{" "}
+            &nbsp;{" "}
+            <a href="https://www.ouorz.com" target="_blank">
+              <UserOutlined /> TonyHe
+            </a>
+          </div>
         </div>
-        <div>
-          <a href="https://github.com/Snapaper" target="_blank">
-            <GithubFilled /> Open-source
-          </a>{" "}
-          &nbsp;{" "}
-          <a href="https://www.ouorz.com" target="_blank">
-            <UserOutlined /> TonyHe
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
+      </footer>
+    );
+  }
 }
 
-export default Footer;
+export default withRouter(Footer);
--- src/pages/cate/alevels.js
+++ src/pages/cate/alevels.js
@@ -1,28 +1,92 @@
-import Link from 'next/link'
-import React from 'react'
-import isMobile from 'ismobilejs'
-import Header from '../../components/header'
-import Footer from '../../components/footer'
+import Link from "next/link";
+import React from "react";
+import isMobile from "ismobilejs";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import { notification, Skeleton } from "antd";
+import { Get } from "react-axios";
 
-export default class Alevel extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {}
-    }
-    componentDidMount(){
-        this.setState({
-            isMobile: isMobile(window.navigator).any
-        })
-    }
-    render(){
+const openNotificationWithIcon = (type, content) => {
+  notification[type]({
+    message: "Notification",
+    description: content,
+  });
+};
+
+export default class Alevel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false,
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      isMobile: isMobile(window.navigator).any,
+    });
+  }
+  render() {
     return (
-        <div>
-            <Header></Header>
-            <main className="ant-container">
-                <h1>Hello World</h1>
-            </main>
-            <Footer></Footer>
-        </div>
-    )
-    }
+      <div>
+        <Header></Header>
+        <main className="ant-container">
+          <Get
+            url="https://www.snapaper.com/vue/cates?cate=A%20Levels"
+            onSuccess={() =>
+              this.setState({
+                display: true,
+              })
+            }
+          >
+            {(error, response, isLoading, onReload) => {
+              if (error) {
+                openNotificationWithIcon(
+                  "error",
+                  "Request error, please report to TonyHe"
+                );
+                return (
+                  <div>
+                    Something bad happened: {error.message}{" "}
+                    <button
+                      onClick={() => onReload({ params: { reload: true } })}
+                    >
+                      Retry
+                    </button>
+                  </div>
+                );
+              } else if (isLoading) {
+                return (
+                  <div>
+                    <Skeleton active />
+                    <Skeleton active />
+                    <Skeleton active />
+                  </div>
+                );
+              } else if (response !== null) {
+                return (
+                  <div>
+                    {response.data.cates.map((item, index) => {
+                      if (!!item.name) {
+                        return (
+                          <div key={index}>{item.name.replace("amp;", "")}</div>
+                        );
+                      }
+                    })}
+                  </div>
+                );
+              }
+              return (
+                <div>
+                  <Skeleton active />
+                  <Skeleton active />
+                  <Skeleton active />
+                </div>
+              );
+            }}
+          </Get>
+        </main>
+        <Footer loading={!this.state.display}></Footer>
+      </div>
+    );
+  }
 }
