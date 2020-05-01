7c2561d7d09e855bf497737927bc2af3e96d77bf
HelipengTony <he@holptech.com>
2020-05-02 00:57:55 +0800

    Add: about page

--- src/pages/page/about.js
+++ src/pages/page/about.js
@@ -1,28 +1,193 @@
-import Link from 'next/link'
-import React from 'react'
-import isMobile from 'ismobilejs'
-import Header from '../../components/header'
-import Footer from '../../components/footer'
+import React from "react";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
 
-export default class Alevel extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {}
-    }
-    componentDidMount(){
-        this.setState({
-            isMobile: isMobile(window.navigator).any
-        })
-    }
-    render(){
+export default class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
     return (
-        <div>
-            <Header></Header>
-            <main className="ant-container">
-                <h1>Hello World</h1>
-            </main>
-            <Footer></Footer>
-        </div>
-    )
-    }
+      <div>
+        <Header></Header>
+        <main className="ant-container">
+          <div className="page-container">
+            <div>
+              <h3 className="uk-card-title index-cate-h3">Resource Guide</h3>
+              <p>
+                This image is from GCE Guide (http://www.gceguide.com)
+                <br />
+                <img
+                  src="https://static.ouorz.com/res_guide.png"
+                  width="90%"
+                  height="auto"
+                />
+              </p>
+              <br />
+
+              <h3 className="uk-card-title index-cate-h3">Copyrights</h3>
+              <p>
+                All Contents & names of this website are assets of owner,
+                protected by law and powered by{" "}
+                <b>
+                  GCE Guide (http://www.gceguide.com) or SaveMyExams
+                  (https://www.savemyexams.co.uk) or Xtremepapers
+                  (https://www.xtremepapers.com)
+                </b>
+              </p>
+              <br />
+
+              <h3 className="uk-card-title index-cate-h3">Technical details</h3>
+              <p>
+                This website (https://www.snapaper.com) does not have a
+                database. All content data comes from GCE Guide
+                (http://www.gceguide.com) or SaveMyExams
+                (https://www.savemyexams.co.uk) or Xtremepapers
+                (https://www.xtremepapers.com). All requests are obtained from
+                the GCE Guide (http://www.gceguide.com) or SaveMyExams
+                (https://www.savemyexams.co.uk) or Xtremepapers
+                (https://www.xtremepapers.com) website. All the content has not
+                been changed or downloaded. This website is based on the GCE
+                Guide (http://www.gceguide.com) or SaveMyExams
+                (https://www.savemyexams.co.uk) or Xtremepapers
+                (https://www.xtremepapers.com) to provide a convenient download
+                extension. This is 100% a non-profit website
+              </p>
+              <br />
+
+              <h3 className="uk-card-title index-cate-h3">Author</h3>
+              <p>
+                This website was developed by <b>TonyHe</b>. Welcome to donate
+                him through <b>Alipay (13408697095)</b>. If you encounter
+                unexpected difficulties during the use of this website, please
+                contact him through <b>QQ (36624065)</b>. If you have
+                suggestions for this website, you can contact him, but he will
+                not necessarily care about you. The source code of this website
+                has been open sourced on Github, welcome to
+                Star/Fork/Watch/Issue
+                <br />
+                <br />
+                <iframe
+                  src="https://ghbtns.com/github-btn.html?user=HelipengTony&repo=snapaper&type=star&count=true&size=large"
+                  frameborder="0"
+                  scrolling="0"
+                  width="160px"
+                  height="30px"
+                ></iframe>
+                <iframe
+                  src="https://ghbtns.com/github-btn.html?user=HelipengTony&repo=snapaper&type=watch&count=true&size=large&v=2"
+                  frameborder="0"
+                  scrolling="0"
+                  width="160px"
+                  height="30px"
+                ></iframe>
+                <iframe
+                  src="https://ghbtns.com/github-btn.html?user=HelipengTony&repo=snapaper&type=fork&count=true&size=large"
+                  frameborder="0"
+                  scrolling="0"
+                  width="158px"
+                  height="30px"
+                ></iframe>
+                <iframe
+                  src="https://ghbtns.com/github-btn.html?user=HelipengTony&type=follow&count=true&size=large"
+                  frameborder="0"
+                  scrolling="0"
+                  width="220px"
+                  height="30px"
+                ></iframe>
+              </p>
+              <br />
+
+              <h3 className="uk-card-title index-cate-h3">Change Logs</h3>
+              <p>
+                <b>V0.1</b>
+                <br />
+                First version released
+                <br />
+                <br />
+                <b>V0.11</b>
+                <br />
+                1. Staticized index page
+                <br />
+                2. Fixed download canceled bug
+                <br />
+                <br />
+                <b>V0.12</b>
+                <br />
+                1. Now using Grid layout in subjects pages
+                <br />
+                2. Fixed a bug where 「Back」 button could not be used under
+                some browsers
+                <br />
+                <br />
+                <b>V0.13</b>
+                <br />
+                1. Fixed a bug where the past papers were not found in some
+                subjects
+                <br />
+                2. Change the domain from papers.ouorz.com to snapaper.com
+                <br />
+                3. Improved papers list button style
+                <br />
+                <br />
+                <b>V0.15</b>
+                <br />
+                1. New menu shortcut download button on papaers page
+                <br />
+                2. Added "Back to Top" and "Back to Bottom" buttons on papers
+                page
+                <br />
+                3. Added a notification page for maintenance
+                <br />
+                4. Added a toolbar to count the number of test papers that have
+                been added to the list on papers page
+                <br />
+                <br />
+                <b>V0.151</b>
+                <br />
+                1.The download function is now available for more browsers
+                <br />
+                <br />
+                <br />
+                <b>V0.16</b>
+                <br />
+                1.UI adjustment
+                <br />
+                2.New index design
+                <br />
+                3.New feature —— Extensions
+                <br />
+                4.New extension —— OneStep
+                <br />
+                5.Added a donate page
+                <br />
+                <br />
+                <b>V0.17</b>
+                <br />
+                1.Added a associated paper shortcut button
+                <br />
+                2.Added a bar to display the total number of items in each page
+                <br />
+                3.Added a Switch Server button
+                <br />
+                4.Paper display page redesigned
+                <br />
+                5.Added a new papers source —— Xtremepapers
+                <br />
+                <br />
+                <b>V0.18</b>
+                <br />
+                1.Rebuilt using Vue.js
+                <br />
+                <br />
+                <br />
+              </p>
+            </div>
+          </div>
+        </main>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
