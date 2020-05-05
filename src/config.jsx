c087d632af2c2f55f5856cc43b383edda32b0d58
HelipengTony <he@holptech.com>
2020-05-05 21:21:40 +0800

    Feature: configurable API links

---
+++ src/config.jsx
@@ -0,0 +1,12 @@
+export default {
+    apiUrl:{
+        cates:{
+            alevel: 'https://www.snapaper.com/api/cates/A%20Levels/',
+            igcse: 'https://www.snapaper.com/api/cates/IGCSE/'
+        },
+        papers:{
+            alevel: 'https://www.snapaper.com/api/papers/A%20Levels/',
+            igcse: 'https://www.snapaper.com/api/papers/IGCSE/'
+        }
+    }
+}
