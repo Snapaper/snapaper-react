481f59a9a33e2678f54c28c8656779d9ff935c88
Tony He <tony.hlp@hotmail.com>
2024-04-10 00:40:12 -0400

    chore: update cors anywhere service url

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -170,7 +170,7 @@ export default class Index extends React.Component {
 			this.setState({
 				oneStepLoading: true,
 			});
-			fetch(`https://cors.snapaper.com/${url}`)
+			fetch(`https://cors.ouorz.com/${url}`)
 				.then((res) => {
 					if (res.status === 200) {
 						//导航至网址
